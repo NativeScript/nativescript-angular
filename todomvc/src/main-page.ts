118400b99e25c35e2e8ff632535868067d741912
Hristo Deshev <hristo@deshev.com>
2015-08-18 13:32:16 +0300

    todomvc: bound styles and switches.

--- todomvc/src/main-page.ts
+++ todomvc/src/main-page.ts
@@ -3,6 +3,7 @@ import {TextView} from 'ui/text-view';
 import {topmost} from 'ui/frame';
 import {nativeScriptBootstrap} from 'nativescript-angular/application';
 import {Inject, Component, View, NgIf, NgFor} from 'angular2/angular2';
+import {TodoStore, Todo} from './services/store';
 
 @Component({
 	selector: 'main',
@@ -13,12 +14,40 @@ import {Inject, Component, View, NgIf, NgFor} from 'angular2/angular2';
     directives: [NgIf, NgFor],
 	template: `
 <StackLayout orientation='vertical'>
-    <Label text='Hello, Angular!' fontSize='20' verticalAlignment='center' padding='20'></Label>
+    <Label text='Hello, Angularrrr!' cssClass='title complete'></Label>
+    <StackLayout orientation='vertical'>
+        <StackLayout
+            *ng-for="#todo of todoStore.todos"
+            orientation='horizontal'
+            [class.complete]="todo.completed" [class.incomplete]="!todo.completed" [class.editing]="todo.editing"
+            >
+            <Switch [checked]="todo.completed" (tap)="toggleCompletion(todo, checked)"></Switch>
+            <Label [text]="todo.title"></Label>
+        </StackLayout>
+    </StackLayout>
+    <Button text='Add' (tap)='onAdd($event)'></Button>
 </StackLayout>
 `,
 })
 class MainPage {
-    constructor() {
+	todoStore: TodoStore;
+
+	constructor() {
+		this.todoStore = new TodoStore();
+        this.todoStore.add("item 1", true);
+        this.todoStore.add("item 2", false);
+	}
+
+    onAdd(eventData) {
+        this.todoStore.add("fafla", false); 
+    }
+
+    toggleCompletion(todo: Todo, checked: boolean) {
+        console.log('toggleCompletion: ' + todo.completed);
+        if (todo.completed !== undefined) {
+            setTimeout(() => todo.completed = !todo.completed, 0);
+        }
+        //setTimeout(() => todo.completed = checked, 0);
     }
 }
 
