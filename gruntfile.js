d9edbee935d8e7d1000ff3f7987b8c9db361b084
Hristo Deshev <hristo@deshev.com>
2015-09-18 16:09:24 +0300

    Use TypeScript 1.6

--- gruntfile.js
+++ gruntfile.js
@@ -40,6 +40,12 @@ module.exports = function(grunt) {
                 outDir: outDir,
                 options: {
                     fast: 'never',
+
+                    // Resolve non-relative modules like "ui/styling/style"
+                    // based on the project root (not on node_modules which
+                    // is the typescript 1.6+ default)
+                    additionalFlags: '--moduleResolution classic',
+
                     module: "commonjs",
                     target: "es5",
                     sourceMap: true,
