19c776f9e5912f37baec8e7020b2b6caeec00cfd
Hristo Deshev <hristo@deshev.com>
2015-11-06 17:40:16 +0200

    Forgot a gitignored file

---
+++ src/global.d.ts
@@ -0,0 +1,39 @@
+declare var assert: any;
+
+interface BrowserNodeGlobal {
+  Object: typeof Object;
+  Array: typeof Array;
+  Map: typeof Map;
+  Set: typeof Set;
+  Date: typeof Date;
+  RegExp: typeof RegExp;
+  JSON: typeof JSON;
+  Math: typeof Math;
+  assert(condition: any): void;
+  Reflect: any;
+  zone: Zone;
+  getAngularTestability: Function;
+  getAllAngularTestabilities: Function;
+  setTimeout: Function;
+  clearTimeout: Function;
+  setInterval: Function;
+  clearInterval: Function;
+}
+
+interface Map<K, V> {
+    keys(): Array<K>;
+    values(): Array<V>;
+}
+
+interface NumberConstructor {
+    isInteger(number: number): boolean;
+}
+
+interface Array<T> {
+    fill(value: T, start?: number, end?: number): T[];
+}
+
+interface String {
+    endsWith(searchString: string, endPosition?: number): boolean;
+    startsWith(searchString: string, position?: number): boolean;
+}
