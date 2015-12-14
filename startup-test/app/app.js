24d704d195c1e0a949686e1ab0c2f09c646c1783
Hristo Deshev <hristo@deshev.com>
2015-12-14 17:00:48 +0200

    startup-app webpack update

--- startup-test/app/app.js
+++ startup-test/app/app.js
@@ -1,19 +1,18 @@
+require("globals");
 var profiling = require('./profiling');
 profiling.start('application-start');
 
 var reflectMetadata = require("reflect-metadata");
-var global = require("globals");
-var page = require("ui/page");
-console.log('page module: ' + page);
 
+//var page = require("ui/page");
+//console.log('page module: ' + page);
 var application = require("application");
 application.on(application.launchEvent, function() {
     //console.log('launched!');
     //profiling.stop('application-start');
 });
 
-var moduleLoader = require("utils/module-loader");
-moduleLoader.registerModule("./main-page", function () { return require("./main-page"); });
+global.registerModule("./main-page", function () { return require("./main-page"); });
 
 application.mainModule = "./main-page";
 application.cssFile = "./app.css";
