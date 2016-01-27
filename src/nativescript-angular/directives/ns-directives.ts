95043dba9d84eb8cfa88740ae42de95d8979eebe
Nedyalko Nikolov <nedyalko.nikolov@telerik.com>
2016-01-27 14:48:59 +0200

    Included TextValueAccessor directive as NS_Directive.

--- src/nativescript-angular/directives/ns-directives.ts
+++ src/nativescript-angular/directives/ns-directives.ts
@@ -1,4 +1,5 @@
 import {Type} from 'angular2/src/facade/lang';
 import {ListItemTemplate, ListViewDirective} from './list-view';
+import { TextValueAccessor } from '../text-value-accessor';
 
-export const NS_DIRECTIVES: Type[] = [ListItemTemplate, ListViewDirective];
+export const NS_DIRECTIVES: Type[] = [ListItemTemplate, ListViewDirective, TextValueAccessor];
