a9bf39e8a13617ab88aa4ee2b60e69a1b7d6e24d
Hristo Deshev <hristo@deshev.com>
2016-02-03 17:29:14 +0200

    tests app first import

---
+++ tests/app/main.ts
@@ -0,0 +1,5 @@
+// this import should be first in order to load some required settings (like globals and reflect-metadata)
+import {nativeScriptBootstrap} from "./nativescript-angular/application";
+import {AppComponent} from "./app.component";
+
+nativeScriptBootstrap(AppComponent);
