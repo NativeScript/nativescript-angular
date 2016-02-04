5812e9330a600e8a946788e4db2606c57e6eb7ba
Hristo Deshev <hristo@deshev.com>
2016-02-04 14:31:12 +0200

    Kill a debug log in the build hooks

--- tests/hooks/before-prepare/10-copy-renderer.js
+++ tests/hooks/before-prepare/10-copy-renderer.js
@@ -2,8 +2,6 @@ var path = require('path');
 var shelljs = require('shelljs');
 
 module.exports = function ($logger, $projectData, $usbLiveSyncService) {
-    console.log(arguments);
-    console.log('LIVESYNC: ' + $usbLiveSyncService.isInitialized);
     if (!$usbLiveSyncService.isInitialized) {
         shelljs.cp('-Rf', '../src/nativescript-angular', 'app');
         shelljs.cp('-Rf', '../src/global.d.ts', 'app');
