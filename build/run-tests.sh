b349d469ca42198e7b67bf04417b547f93e8dfa5
Hristo Deshev <hristo@deshev.com>
2016-02-05 16:57:39 +0200

    Script running Android tests with the karma runner.

---
+++ build/run-tests.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+set -e
+
+SCRIPT=$(readlink -f "$0")
+BUILD_DIR=$(dirname $SCRIPT)
+PROJECT_DIR=$(dirname $BUILD_DIR)
+
+$PROJECT_DIR/build/start-android-emulator.sh  &
+EMULATOR_STARTER_PID=$!
+
+. $PROJECT_DIR/build/tns.sh
+
+activate_node_env
+
+cd "$PROJECT_DIR/tests"
+
+npm install
+rm -rf platforms/android
+tns platform add android
+
+wait $EMULATOR_STARTER_PID
+tns test android | tee test-output.txt
+
+if grep -q -E 'Executed[[:space:]][[:digit:]]+.*FAILED' test-output.txt ; then
+    echo "TESTS FAILED!"
+    exit 1
+else
+    echo "TESTS SUCCEEDED."
+    exit 0
+fi
---
+++ build/tns.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+set -e
+
+export NVM_REPO="https://github.com/creationix/nvm.git"
+export NVM_DIR="$(pwd)/.nvm"
+export NODE_VER="v5.5.0"
+
+install_nvm() {
+    if [ ! -d "$NVM_DIR" ] ; then
+        git clone --depth=1 $NVM_REPO $NVM_DIR
+    else
+        echo "NVM detected at $NVM_DIR"
+    fi
+}
+
+activate_nvm() {
+    . "$NVM_DIR/nvm.sh"
+}
+
+installed_under_nvm() {
+    which $1 | grep -q "$NVM_DIR"
+}
+
+install_node() {
+    nvm install $NODE_VER
+    nvm use $NODE_VER
+}
+
+
+install_latest_tns() {
+    if installed_under_nvm tns ; then
+        echo "tns already installed."
+    else
+        echo "n" | npm install -g nativescript
+    fi
+}
+
+activate_node_env() {
+    install_nvm
+    activate_nvm
+    install_node
+    install_latest_tns
+}
