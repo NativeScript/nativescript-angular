f44bc1a126fc78c57285fb0a0c44ff8218261823
Hristo Deshev <hristo@deshev.com>
2016-02-18 11:29:29 +0200

    Upgrade tests app

--- tests/app/global.d.ts
+++ tests/app/global.d.ts
@@ -1,5 +1,4 @@
 /// <reference path="../node_modules/tns-core-modules/tns-core-modules.d.ts" />
-declare var assert: any;
 
 interface Map<K, V> {
     keys(): Array<K>;
@@ -8,19 +7,3 @@ interface Map<K, V> {
 
 declare type MapConstructor = typeof Map;
 declare type SetConstructor = typeof Set;
-
-interface NumberConstructor {
-    isInteger(number: number): boolean;
-}
-
-interface Array<T> {
-    fill(value: T, start?: number, end?: number): T[];
-}
-
-interface String {
-    endsWith(searchString: string, endPosition?: number): boolean;
-    startsWith(searchString: string, position?: number): boolean;
-}
-
-interface Zone {
-}
