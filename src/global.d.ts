8ea07147dca614da890237d7a9ecf2251f4b3409
Hristo Deshev <hristo@deshev.com>
2016-03-16 16:02:03 +0200

    Upgrade to {N} 1.7

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,14 +1,1 @@
 /// <reference path="../node_modules/tns-core-modules/tns-core-modules.d.ts" />
-
-//Compatibility interfaces for rxjs
-
-interface IteratorResult<T> {
-    done: boolean;
-    value?: T;
-}
-
-interface Iterator<T> {
-    next(value?: any): IteratorResult<T>;
-    return?(value?: any): IteratorResult<T>;
-    throw?(e?: any): IteratorResult<T>;
-}
