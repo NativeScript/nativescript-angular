2da570db7c6ffefd3b97e6d5f70830f02dacef43
vakrilov <alexander.vakrilov@telerik.com>
2016-04-06 16:16:12 +0300

    Gestures snippets

--- tests/app/main.ts
+++ tests/app/main.ts
@@ -1,5 +1,7 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import {nativeScriptBootstrap} from "nativescript-angular/application";
 import {AppComponent} from "./app.component";
+import {GestureComponent} from "./snippets/gestures.component";
 
 nativeScriptBootstrap(AppComponent);
+// nativeScriptBootstrap(GestureComponent);
---
+++ tests/app/snippets/gestures.component.ts
@@ -0,0 +1,65 @@
+import {Component} from "angular2/core";
+import {
+    GestureEventData,
+    PanGestureEventData,
+    PinchGestureEventData,
+    RotationGestureEventData,
+    SwipeGestureEventData,
+    TouchGestureEventData} from "ui/gestures";
+
+@Component({
+    selector: "gestures",
+    templateUrl: "snippets/gestures.component.xml",
+    styles:['label { font-size: 32; margin: 2; background-color: lightgreen;}']
+})
+export class GestureComponent {
+    
+    // >> tap-gesture
+    onTap(args: GestureEventData) {
+        console.log("Tap!")
+    }
+    // << tap-gesture
+    
+    // >> double-tap-gesture
+    onDoubleTap(args: GestureEventData) {
+        console.log("DoubleTap!")
+
+    }
+    // << double-tap-gesture
+    
+    // >> long-press-gesture
+    onLongPress(args: GestureEventData) {
+        console.log("LongPress!")
+    }
+    // << long-press-gesture
+    
+    // >> swipe-gesture
+    onSwipe(args: SwipeGestureEventData) {
+        console.log("Swipe Direction: " + args.direction);
+    }
+    // << swipe-gesture
+    
+    // >> pan-gesture
+    onPan(args: PanGestureEventData) {
+        console.log("Pan delta: [" + args.deltaX + ", " + args.deltaY + "] state: " + args.state);
+    }
+    // << pan-gesture
+    
+    // >> pinch-gesture
+    onPinch(args: PinchGestureEventData) {
+        console.log("Pinch scale: " + args.scale + " state: " + args.state);
+    }
+    // << pinch-gesture
+    
+    // >> rotate-gesture
+    onRotate(args: RotationGestureEventData) {
+        console.log("Rotate angle: " + args.rotation + " state: " + args.state);
+    }
+    // << rotate-gesture
+    
+    // >> touch-gesture
+    onTouch(args: TouchGestureEventData) {
+        console.log("Touch point: [" + args.getX() + ", " + args.getY() + "] activePointers: " + args.getActivePointers().length);
+    } 
+    // << touch-gesture   
+}
