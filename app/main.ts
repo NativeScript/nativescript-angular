9537a4bea1bbc567954d611e36b64a18146be609
vchimev <Vasil.Chimev@telerik.com>
2016-05-18 17:36:26 +0300

    Add action-bar test components

--- app/main.ts
+++ app/main.ts
@@ -1,15 +1,18 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import {nativeScriptBootstrap} from "nativescript-angular/application";
 import {NS_ROUTER_PROVIDERS} from "nativescript-angular/router";
-
+import {ActionBarTest} from "./action-bar/action-bar-test"
 import {AppComponent} from "./template/app.component";
 import {NavigationTestRouter} from "./router/router-outlet"
 import {NavigationTestPageRouter} from "./router/page-router-outlet"
 import {NavigationMainPageRouter} from "./main/main-page-router-outlet"
 
-// nativeScriptBootstrap(AppComponent);
+// action-bar
+// nativeScriptBootstrap(ActionBarTest, [NS_ROUTER_PROVIDERS]);
+// nativeScriptBootstrap(ActionBarTest, [NS_ROUTER_PROVIDERS], { startPageActionBarHidden: true });
 
 // tns-template-hello-world-ng
+// nativeScriptBootstrap(AppComponent);
 // nativeScriptBootstrap(AppComponent, null, {"cssFile": "./template/app.component.css"});
 
 // router-outlet
--- app/main/main-page-router-outlet.ts
+++ app/main/main-page-router-outlet.ts
@@ -1,11 +1,30 @@
 import {Component} from "@angular/core";
 import {RouteConfig} from '@angular/router-deprecated';
 import {NS_ROUTER_DIRECTIVES} from "nativescript-angular/router";
+import {ActionBarTest} from "../action-bar/action-bar-test"
 import {AppComponent} from "../template/app.component";
-import {MainComponent} from "./main.component";
 import {FirstComponent} from "../components/first.component";
 import {SecondComponent} from "../components/second.component";
 import {NavigationTestRouter} from "../router/router-outlet";
+
+@Component({
+    selector: "main",
+    directives: [NS_ROUTER_DIRECTIVES],
+    template: `
+    <StackLayout>
+        <Label text="Main Component" class="title"></Label>
+        <Button text="Template" [nsRouterLink]="['Template']"></Button>
+        <Button text="First" [nsRouterLink]="['First']"></Button>      
+        <Button text="Second" [nsRouterLink]="['Second']"></Button>
+        <Button text="Router" [nsRouterLink]="['Router']"></Button>
+        <StackLayout orientation="horizontal" horizontalAlignment="center">
+            <Button text="ActionBar" [nsRouterLink]="['ActionBar']"></Button>
+            <Button text="Hidden" [nsRouterLink]="['ActionBar']"></Button>
+        </StackLayout>
+    </StackLayout>
+    `,
+})
+class MainComponent {}
 
 @Component({
     selector: 'navigation-main',
@@ -17,6 +36,7 @@ import {NavigationTestRouter} from "../router/router-outlet";
     { path: '/template', component: AppComponent, name: 'Template' },
     { path: '/first', component: FirstComponent, name: 'First' },
     { path: '/second', component: SecondComponent, name: 'Second' },
-    { path: '/router/...', component: NavigationTestRouter, name: 'Router' }
+    { path: '/router/...', component: NavigationTestRouter, name: 'Router' },
+    { path: '/action-bar-test/...', component: ActionBarTest, name: 'ActionBar' },
 ])
 export class NavigationMainPageRouter {}
