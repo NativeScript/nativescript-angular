78d5d92f36da2b89c5d3c3a7c9305d9c587818c2
vakrilov <alexander.vakrilov@telerik.com>
2016-07-06 10:45:46 +0300

    Migrate to RC4

--- nativescript-angular/router/ns-router.ts
+++ nativescript-angular/router/ns-router.ts
@@ -1,14 +1,13 @@
 import {Type} from '@angular/core/src/facade/lang';
-import {provide, Injectable} from '@angular/core';
-import {LocationStrategy, PlatformLocation, UrlChangeListener} from '@angular/common';
+import {provide} from '@angular/core';
+import {LocationStrategy, PlatformLocation} from '@angular/common';
 import { RouterConfig } from '@angular/router';
-import { provideRouter, ExtraOptions } from '@angular/router/common_router_providers';
+import { provideRouter, ExtraOptions } from '@angular/router/src/common_router_providers';
 
 import {NSRouterLink} from './ns-router-link';
 import {PageRouterOutlet} from './page-router-outlet';
 import {NSLocationStrategy} from './ns-location-strategy';
 import {NativescriptPlatformLocation} from './ns-platform-location';
-import {routerLog} from "../trace";
 
 export {routerTraceCategory} from "../trace";
 
@@ -28,6 +27,6 @@ export const NS_ROUTER_DIRECTIVES: Type[] = [
 export function nsProvideRouter(config: RouterConfig, opts: ExtraOptions): any[] {
     return [
         ...NS_ROUTER_PROVIDERS,
-        ...provideRouter(config, opts)
-    ]
+        ...provideRouter(config, opts) // NOTE: use provideRouter form common_router_providers - it doesnt include BrowserPlatformLocation
+    ];
 };
