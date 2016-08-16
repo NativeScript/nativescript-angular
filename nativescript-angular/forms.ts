dd610a127c9639f9c79ac4bf7e1e76c57b587413
Hristo Deshev <hristo@deshev.com>
2016-08-16 14:06:42 +0300

    Add forms module. Throw in an empty router module too.

---
+++ nativescript-angular/forms.ts
@@ -0,0 +1,33 @@
+import { Type } from '@angular/core/src/facade/lang';
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { TextValueAccessor } from './value-accessors/text-value-accessor';
+import { CheckedValueAccessor } from './value-accessors/checked-value-accessor';
+import { DateValueAccessor } from './value-accessors/date-value-accessor';
+import { TimeValueAccessor } from './value-accessors/time-value-accessor';
+import { NumberValueAccessor } from './value-accessors/number-value-accessor';
+import { SelectedIndexValueAccessor } from './value-accessors/selectedIndex-value-accessor';
+
+export const FORMS_DIRECTIVES: Type[] = [
+    TextValueAccessor,
+    CheckedValueAccessor,
+    DateValueAccessor,
+    TimeValueAccessor,
+    SelectedIndexValueAccessor,
+    NumberValueAccessor,
+];
+
+@NgModule({
+    declarations: FORMS_DIRECTIVES,
+    providers: [
+    ],
+    imports: [
+        FormsModule
+    ],
+    exports: [
+        FormsModule,
+        FORMS_DIRECTIVES,
+    ]
+})
+export class NativeScriptFormsModule {
+}
--- nativescript-angular/router.ts
+++ nativescript-angular/router.ts
@@ -1,1 +1,16 @@
+import { NgModule } from "@angular/core";
+
 export * from "./router/ns-router";
+
+@NgModule({
+    declarations: [
+    ],
+    providers: [
+    ],
+    imports: [
+    ],
+    exports: [
+    ]
+})
+export class NativeScriptRouterModule {
+}
