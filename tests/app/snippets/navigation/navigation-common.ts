552b6072fc332538aa0546a1444faac314e9749a
Tsvetan Raikov <tsvetan.raikov@telerik.com>
2016-10-25 18:52:03 +0300

    Updated router documentation code snippets

--- tests/app/snippets/navigation/navigation-common.ts
+++ tests/app/snippets/navigation/navigation-common.ts
@@ -1,23 +1,25 @@
 // >> router-content-components
-import {Component} from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
     selector: "first",
     template: `
-    <StackLayout>
-        <Label text="First component" class="title"></Label>
-        <Button text="GO TO SECOND" [nsRouterLink]="['/second']" class="link"></Button>
-    </StackLayout>`
+        <StackLayout>
+            <Label text="First component" class="title"></Label>
+            <Button text="GO TO SECOND" [nsRouterLink]="['/second']" class="link"></Button>
+        </StackLayout>
+    `
 })
 export class FirstComponent { }
 
 @Component({
     selector: "second",
     template: `
-    <StackLayout>
-        <Label text="Second component" class="title"></Label>
-        <Button text="GO TO FIRST" [nsRouterLink]="['/first']" class="link"></Button>
-    </StackLayout>`
+        <StackLayout>
+            <Label text="Second component" class="title"></Label>
+            <Button text="GO TO FIRST" [nsRouterLink]="['/first']" class="link"></Button>
+        </StackLayout>
+    `
 })
 export class SecondComponent { }
 // << router-content-components
