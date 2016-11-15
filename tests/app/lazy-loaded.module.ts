c1816ce3a010935cd5b3074edd6944ee92238576
sis0k0 <stanimira.vlaeva@gmail.com>
2016-11-15 14:14:12 +0200

    tests(lazy-loading): add e2e test for lazy-loaded module

---
+++ tests/app/lazy-loaded.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { NativeScriptModule } from "nativescript-angular";
+import { NativeScriptRouterModule } from "nativescript-angular/router";
+
+import { SecondComponent } from './second.component';
+
+const routes = [
+    { path: ":id", component: SecondComponent },
+];
+
+@NgModule({
+    declarations: [SecondComponent],
+    imports: [
+        NativeScriptModule,
+        NativeScriptRouterModule,
+        NativeScriptRouterModule.forChild(routes)
+    ]
+})
+export class SecondModule { }
+
