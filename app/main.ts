e21ab594af36afbc7caf5bd04a24be4a20438cc7
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2017-03-15 10:46:21 +0200

    Rebase on top of release (#14)
    * fix: import NativeScriptModule from nativescript-angular/nativescript.module (#13)
    
    * Add TabView example page.
    
    * Upgrade to TypeScript 2.2
    
    * Upgrade tsconfig for latest d.ts changes.

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,6 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
-import { platformNativeScriptDynamic, NativeScriptModule } from "nativescript-angular/platform";
+import { platformNativeScriptDynamic } from "nativescript-angular/platform";
+import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { NgModule } from "@angular/core";
 import { NavigationMainPageRouter } from "./main/main-page-router-outlet";
 import { routableComponents, routes } from "./app.routes";
