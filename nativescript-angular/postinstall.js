0ace0ddfc50aa3a51ac7c4cd5e7b6cf58dd1ad8b
sis0k0 <stanimira.vlaeva@gmail.com>
2017-03-23 13:31:03 +0200

    Revert "build: fix animations bundle on postinstall"
    This reverts commit 960702bdc4acc90a43693010229dc5f15051287c.

--- nativescript-angular/postinstall.js
+++ nativescript-angular/postinstall.js
@@ -1,21 +1,9 @@
 var fs = require("fs");
 var os = require("os");
-var path = require("path");
 var hookHelper = require("./hooks/hook-helper");
 var projectDir = hookHelper.findProjectDir();
 
 if (projectDir) {
-    var bundlePath = path.join(projectDir, "node_modules/@angular/animations/browser/package.json");
-
-    try {
-        var content = require(bundlePath);
-        content.main = "../bundles/animations-browser.umd.js";
-        console.log(content)
-        fs.writeFileSync(bundlePath, JSON.stringify(content), "utf8");
-    } catch(e) {
-        console.error(e.message);
-    }
-
     var hooksDir = hookHelper.getHooksDir(),
         beforeLivesyncHookDir = hookHelper.getBeforeLivesyncHookDir(),
         content = 'module.exports = require("nativescript-angular/hooks/before-livesync");';
