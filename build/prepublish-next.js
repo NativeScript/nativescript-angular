7969b1a19dd909402333391541bd5725aea4e87c
vchimev <Vasil.Chimev@telerik.com>
2017-07-03 14:07:05 +0300

    chore(travis): do not publish next version Publish next version from Jenkins CI.

---
+++ build/prepublish-next.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+const fs = require("fs");
+const path = require("path");
+
+const { dirname: pathDirname, join } = require("path");
+
+// helpers start
+const getPackageJson = projectDir => {
+    const packageJsonPath = getPackageJsonPath(projectDir);
+    return JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+};
+
+const writePackageJson = (content, projectDir) => {
+    const packageJsonPath = getPackageJsonPath(projectDir);
+    fs.writeFileSync(packageJsonPath, JSON.stringify(content, null, 2))
+}
+
+const getPackageJsonPath = projectDir => path.resolve(projectDir, "package.json");
+// helpers end
+
+const tag = "next";
+const projectDir = pathDirname(join("nativescript-angular","nativescript-angular"));
+const packageJson = getPackageJson(projectDir);
+const [, , packageVersion = new Date() ] = process.argv;
+
+packageJson.publishConfig = Object.assign(
+    packageJson.publishConfig || {},
+    { tag }
+);
+
+delete packageJson.private;
+
+const currentVersion = packageJson.version;
+const nextVersion = `${currentVersion}-${packageVersion}`;
+const newPackageJson = Object.assign(packageJson, { version: nextVersion });
+
+writePackageJson(newPackageJson, projectDir);
