cfc1371d2bf1b8bdeaf72c456a0d22d4ef798ee1
sis0k0 <stanimira.vlaeva@gmail.com>
2017-07-08 12:57:59 +0300

    refactor: update states animations example

--- ng-sample/app/examples/animation/animation-states-test.ts
+++ ng-sample/app/examples/animation/animation-states-test.ts
@@ -16,11 +16,18 @@ import {
     animations: [
         trigger("state", [
 
-            state("inactive", style({ "background-color": "red" })),
-            state("active", style({ "backgroundColor": "green" })),
+            state("inactive", style({
+                backgroundColor: "red",
+                transform: "scale(0.7)",
+            })),
+            state("active", style({
+                backgroundColor: "green",
+                transform: "scale(2)",
+            })),
 
             transition("* => active", [ animate("600ms ease-out") ]),
             transition("* => inactive", [ animate("600ms ease-out") ]),
+
         ])
     ]
 })
