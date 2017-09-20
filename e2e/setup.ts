17fb2636625c930a8e08a5774ef0c0fb211250e0
Vasil Chimev <Vasil.Chimev@telerik.com>
2017-09-20 06:05:01 -0400

    test: update to nativescript-dev-appium@3.0.0 (#1)

--- e2e/setup.ts
+++ e2e/setup.ts
@@ -1,22 +1,9 @@
-import * as setup from "nativescript-dev-appium";
-import * as portastic from "portastic";
+import { startServer, stopServer } from "nativescript-dev-appium";
 
-before("setup server", async () => {
-    console.log("Setting up server");
-    const port = 9191;
-    await setup.startAppiumServer(port);
-    console.log("Server is started");
+before("start server", async () => {
+    await startServer();
 });
 
-before("setup driver", async () => {
-    console.log("Setting up driver");
-});
-
-after("kill driver", async () => {
-    console.log("Kill driver");
-});
-
-after("kill server", async () => {
-    await setup.killAppiumServer();
-    console.log("Server stopped");
+after("stop server", async () => {
+    await stopServer();
 });
---
+++ e2e/tests.e2e.ts
@@ -0,0 +1,40 @@
+import { AppiumDriver, createDriver, SearchOptions } from "nativescript-dev-appium";
+import { assert } from "chai";
+
+describe("sample scenario", () => {
+    const defaultWaitTime = 5000;
+    let driver: AppiumDriver;
+
+    before(async () => {
+        driver = await createDriver();
+    });
+
+    after(async () => {
+        await driver.quit();
+        console.log("Quit driver!");
+    });
+
+    afterEach(async function () {
+        if (this.currentTest.state === "failed") {
+            await driver.logScreenshoot(this.currentTest.title);
+        }
+    });
+
+    it("should go to support page", async () => {
+        const btnGoToSupportPage = await driver.findElementByText("go to support page", SearchOptions.exact);
+        const homeImage = await driver.compareScreen("home");
+        assert.isTrue(homeImage);
+        await btnGoToSupportPage.click();
+        const titleSupportPage = await driver.findElementByText("Support Page", SearchOptions.exact);
+        console.log(await titleSupportPage.text());
+    });
+
+    it("should go back to home page", async () => {
+        const btnGoBackToHomePage = await driver.findElementByText("go back to home page", SearchOptions.exact);
+        const supportImage = await driver.compareScreen("support");
+        assert.isTrue(supportImage);
+        await btnGoBackToHomePage.click();
+        const titleHomePage = await driver.findElementByText("Home Page", SearchOptions.exact);
+        console.log(await titleHomePage.text());
+    });
+});
