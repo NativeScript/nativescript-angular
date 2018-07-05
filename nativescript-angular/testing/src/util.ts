4e9db8b713d547fdd08052090fd55ae9e2ad7cb1
vakrilov <alexander.vakrilov@telerik.com>
2018-07-05 13:23:25 +0300

    feat(test-bed): Run render fixtures in a full-page container

--- nativescript-angular/testing/src/util.ts
+++ nativescript-angular/testing/src/util.ts
@@ -9,12 +9,36 @@ import { platformBrowserDynamicTesting } from "@angular/platform-browser-dynamic
 import { NS_COMPILER_PROVIDERS } from "../../platform";
 import { NATIVESCRIPT_TESTING_PROVIDERS, NativeScriptTestingModule } from "../index";
 import { CommonModule } from "@angular/common";
+import { GridLayout } from "tns-core-modules/ui/layouts/grid-layout";
+
+const TESTING_ROOT_ID = "__testing_container";
+
 /**
- * Get a reference to the root application view.
+ * Get a reference to the fixtures container.
  */
-export function testingRootView(): View {
-    return topmost().currentPage.content;
+export function testingRootView(): LayoutBase {
+    const rootPageLayout = topmost().currentPage.content as LayoutBase;
+
+    let testingRoot: LayoutBase;
+    rootPageLayout.eachChild(child => {
+        if (child.id === TESTING_ROOT_ID) {
+            testingRoot = child as LayoutBase;
+            return false;
+        }
+        return true;
+    });
+
+    if (!testingRoot) {
+        testingRoot = new GridLayout();
+        testingRoot.id = TESTING_ROOT_ID;
+        GridLayout.setColumnSpan(testingRoot, 100);
+        GridLayout.setRowSpan(testingRoot, 100);
+        rootPageLayout.addChild(testingRoot);
+    }
+
+    return testingRoot;
 }
+
 
 /**
  * Declared test contexts. When the suite is done this map should be empty if all lifecycle
--- tests/app/tests/test-config.ts
+++ tests/app/tests/test-config.ts
@@ -1,6 +1,6 @@
-(<any>global).mocha.setup({
-    timeout: 20000,
-});
+(<any>global).mocha.setup(
+    { timeout: 20000 },
+    { exit: true });
 
 import * as chai from "chai";
 export let assert: typeof chai.assert = (<any>global).chai.assert;
