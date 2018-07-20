2defebb29c41641a810fc10f7569b3cd1f6694ce
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2018-07-20 18:44:18 +0300

    chore: typo

--- app/hero/hero-team-builder.component.ts
+++ app/hero/hero-team-builder.component.ts
@@ -5,7 +5,6 @@ import { Heroes } from "./hero.service";
 @Component({
     moduleId: module.id,
     selector: "hero-team-builder",
-    styleUrls: ["./hero-team-builder.component.css"],
     templateUrl: "./hero-team-builder.component.html",
     providers: [Heroes]
 })
