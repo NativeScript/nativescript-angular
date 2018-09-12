940e37dba28d1bcd34fd98d02cdc4a3511ef5ef2
ADjenkov <ADjenkov>
2018-09-12 14:21:49 +0300

    feat(router-extensions): extend back() to handle specific outlets by relative route

--- nativescript-angular/router/router-extensions.ts
+++ nativescript-angular/router/router-extensions.ts
@@ -1,9 +1,14 @@
 import { Injectable } from "@angular/core";
-import { Router, UrlTree, NavigationExtras } from "@angular/router";
-import { NSLocationStrategy, NavigationOptions } from "./ns-location-strategy";
+import { Router, UrlTree, NavigationExtras, ActivatedRoute } from "@angular/router";
+import { NSLocationStrategy, NavigationOptions, Outlet } from "./ns-location-strategy";
 import { FrameService } from "../platform-providers";
 
 export type ExtendedNavigationExtras = NavigationExtras & NavigationOptions;
+
+export interface BackNavigationOptions {
+    outlets?: Array<string>;
+    relativeTo?: ActivatedRoute | null;
+}
 
 @Injectable()
 export class RouterExtensions {
@@ -28,8 +33,12 @@ export class RouterExtensions {
         return this.router.navigateByUrl(url);
     }
 
-    public back() {
-        this.locationStrategy.back();
+    public back(backNavigationOptions?: BackNavigationOptions) {
+        if (backNavigationOptions) {
+            this.backOutlets(backNavigationOptions);
+        } else {
+            this.locationStrategy.back();
+        }
     }
 
     public canGoBack() {
@@ -42,5 +51,35 @@ export class RouterExtensions {
 
     public canGoBackToPreviousPage(): boolean {
         return this.frameService.getFrame().canGoBack();
+    }
+
+    private backOutlets(options: BackNavigationOptions) {
+        const rootRoute: ActivatedRoute = this.router.routerState.root;
+        const outletsToBack: Array<Outlet> = [];
+        const outlets = options.outlets || ["primary"];
+        let relativeRoute: ActivatedRoute = options.relativeTo;
+
+        if (!options.outlets && relativeRoute) {
+            relativeRoute = relativeRoute.parent || rootRoute;
+        } else if (!relativeRoute) {
+            relativeRoute = rootRoute;
+        }
+
+        for (let index = 0; index < relativeRoute.children.length; index++) {
+            const currentRoute = relativeRoute.children[index];
+
+            if (outlets.some(currentOutlet => currentOutlet === currentRoute.outlet)) {
+                const pathToOutlet = this.locationStrategy.getPathToOutlet(currentRoute);
+                outletsToBack.push(this.locationStrategy.findOutlet(pathToOutlet));
+            }
+        }
+
+        if (outletsToBack.length !== outlets.length) {
+            throw new Error("No outlet found relative to activated route");
+        }
+
+        outletsToBack.forEach(outletToBack => {
+            this.locationStrategy.back(outletToBack);
+        });
     }
 }
