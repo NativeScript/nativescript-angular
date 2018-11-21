571b39fd9ab28e0755108da4b5568db2d920da60
ADjenkov <ADjenkov>
2018-11-21 17:05:09 +0200

    tets: add tests for actionBarVisibility and lazy loaded named outlet

---
+++ e2e/renderer/app/page-router-outlet/nested-lazy-page.module.ts
@@ -0,0 +1,20 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptRouterModule } from "nativescript-angular/router";
+import { NativeScriptCommonModule } from "nativescript-angular/common";
+import { SharedModule } from "~/shared.module";
+import { NestedPageComponent } from "./nested-page.component";
+
+@NgModule({
+    imports: [
+        SharedModule,
+        NativeScriptCommonModule,
+        NativeScriptRouterModule,
+        NativeScriptRouterModule.forChild([
+            { path: "", component: NestedPageComponent }
+        ])
+    ],
+    providers: [
+    ],
+    schemas: [NO_ERRORS_SCHEMA]
+})
+export class NestedLazyPageModule { }
--- e2e/renderer/e2e/page-router-outlet.e2e-spec.ts
+++ e2e/renderer/e2e/page-router-outlet.e2e-spec.ts
@@ -93,6 +93,48 @@ describe("page-router-outlet-scenario", () => {
         });
     });
 
+    describe("actionBarVisibility 'never' doesn't show action bars in lazy module page", async () => {
+        before(async () => {
+            driver = await createDriver();
+            await driver.driver.resetApp();
+        });
+
+        afterEach(async function () {
+            if (this.currentTest.state === "failed") {
+                await driver.logTestArtifacts(this.currentTest.title);
+            }
+        });
+
+        it("should navigate to page", async () => {
+            const navigationButton =
+                await driver.findElementByAutomationText("ActionBarVisibility Never Lazy");
+            await navigationButton.click();
+
+            await driver.findElementByAutomationText("ShowActionBar");
+        });
+
+        it("should hide action bar by default", async () => {
+            const screenMatches =  await driver.compareScreen("actionBarVisibility-never-default", 5);
+            assert(screenMatches);
+        });
+
+        it("should not show action bar when shown by page", async () => {
+            const showActionBarButton = await driver.findElementByAutomationText("ShowActionBar");
+            showActionBarButton.click();
+
+            const screenMatches =  await driver.compareScreen("actionBarVisibility-never-shown", 5);
+            assert(screenMatches);
+        });
+
+        it("should not do anything when hidden action bar by page", async () => {
+            const hideActionBarButton = await driver.findElementByAutomationText("HideActionBar");
+            hideActionBarButton.click();
+
+            const screenMatches =  await driver.compareScreen("actionBarVisibility-never-hidden", 5);
+            assert(screenMatches);
+        });
+    });
+
     describe("actionBarVisibility 'auto' shows action bars based on page", async () => {
         before(async () => {
             driver = await createDriver();
