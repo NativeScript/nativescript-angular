c12d2e5bbaaa90918a3da4e7876a9970341d110d
tgpetrov <Todor.Petrov@telerik.com>
2019-08-09 17:03:28 +0300

    refactor: remove http module

--- nativescript-angular/index.ts
+++ nativescript-angular/index.ts
@@ -5,7 +5,6 @@ export * from "./platform";
 export * from "./platform-static";
 export * from "./router";
 export * from "./forms";
-export * from "./http";
 export * from "./directives";
 export * from "./common/detached-loader";
 export * from "./trace";
