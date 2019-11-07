713d0614ef00754a83dd19fc2d28ec7fb28da727
VladimirAmiorkov <vladimir.amiorkov@progress.com>
2019-11-07 18:37:23 +0200

    chore: update ivy sample to use scopped nativescript angular package

--- e2e/ivy-sample/src/app/app.module.ts
+++ e2e/ivy-sample/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { NativeScriptModule } from "@nativescript/angular/nativescript.module";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -7,10 +7,10 @@ import { ItemsComponent } from "./item/items.component";
 import { ItemDetailComponent } from "./item/item-detail.component";
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
-// import { NativeScriptFormsModule } from "nativescript-angular/forms";
+// import { NativeScriptFormsModule } from "@nativescript/angular/forms";
 
 // Uncomment and add to NgModule imports if you need to use the HttpClient wrapper
-// import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
+// import { NativeScriptHttpClientModule } from "@nativescript/angular/http-client";
 
 @NgModule({
     bootstrap: [
