2bbba164364ddb5e1ca11bf6f04eb4a35c6f0366
VladimirAmiorkov <vladimir.amiorkov@progress.com>
2019-11-07 18:37:23 +0200

    chore: update ivy sample to use scopped nativescript angular package

--- e2e/ivy-sample/src/app/app-routing.module.ts
+++ e2e/ivy-sample/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from "@angular/core";
-import { NativeScriptRouterModule } from "nativescript-angular/router";
+import { NativeScriptRouterModule } from "@nativescript/angular/router";
 import { Routes } from "@angular/router";
 
 import { ItemsComponent } from "./item/items.component";
