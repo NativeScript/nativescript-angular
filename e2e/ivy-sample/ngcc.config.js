a3b4c501da4b35b91de6a7c33fbeb2e0effb100d
VladimirAmiorkov <vladimir.amiorkov@progress.com>
2019-11-21 09:25:14 +0200

    chore: update to Angular 9.0.0 RC3 chore: add top plugins to Ivy example chore: add app-wide ngcc.cofnig.js in Ivy example for all included {N} plugins

--- e2e/ivy-sample/ngcc.config.js
+++ e2e/ivy-sample/ngcc.config.js
@@ -1,15 +1,211 @@
-// module.exports = {
-//     "packages": {
-//       "@nativescript/angular": {
-//         "entryPoints": {
-//           ".": {
-//             "override": {
-//               "main": "./index.js",
-//               "typings": "./index.d.ts",
-//             },
-//               "ignoreMissingDependencies": true,
-//           },
-//         }
-//       }
-//     }
-//   }
+module.exports = {
+    "packages": {
+        "@nativescript/angular": {
+            "entryPoints": {
+                ".": {
+                    "override": {
+                        "main": "./index.js",
+                        "typings": "./index.d.ts",
+                    },
+                    "ignoreMissingDependencies": true,
+                },
+            }
+        },
+        "nativescript-bottom-navigation": {
+            "entryPoints": {
+                ".": {
+                    "ignoreMissingDependencies": true,
+                },
+                "angular": {
+                    "override": {
+                        "main": "./index.js",
+                        "typings": "./index.d.ts",
+                    },
+                    "ignoreMissingDependencies": true,
+                },
+            },
+        },
+        "nativescript-drop-down": {
+            "entryPoints": {
+                ".": {
+                    "ignoreMissingDependencies": true,
+                },
+                "angular": {
+                    "override": {
+                        "main": "./index.js",
+                        "typings": "./index.d.ts",
+                    },
+                    "ignoreMissingDependencies": true,
+                },
+            },
+        },
+        "nativescript-grid-view": {
+            "entryPoints": {
+                ".": {
+                    "ignoreMissingDependencies": true,
+                },
+                "angular": {
+                    "override": {
+                        "main": "./index.js",
+                        "typings": "./index.d.ts",
+                    },
+                    "ignoreMissingDependencies": true,
+                },
+            },
+        },
+        "nativescript-pager": {
+            "entryPoints": {
+                ".": {
+                    "ignoreMissingDependencies": true,
+                },
+                "angular": {
+                    "override": {
+                        "main": "./index.js",
+                        "typings": "./index.d.ts",
+                    },
+                    "ignoreMissingDependencies": true,
+                },
+            },
+        },
+        "nativescript-socketio": {
+            "entryPoints": {
+                ".": {
+                    "ignoreMissingDependencies": true,
+                },
+                "angular": {
+                    "override": {
+                        "main": "./index.js",
+                        "typings": "./index.d.ts",
+                    },
+                    "ignoreMissingDependencies": true,
+                },
+            },
+        },
+        "nativescript-ui-autocomplete": {
+            "entryPoints": {
+                ".": {
+                    "ignoreMissingDependencies": true,
+                },
+                "angular": {
+                    "override": {
+                        "main": "./autocomplete-directives.js",
+                        "typings": "./autocomplete-directives.d.ts",
+                    },
+                    "ignoreMissingDependencies": true,
+                },
+            },
+        },
+        "nativescript-ui-calendar": {
+            "entryPoints": {
+                ".": {
+                    "ignoreMissingDependencies": true,
+                },
+                "angular": {
+                    "override": {
+                        "main": "./calendar-directives.js",
+                        "typings": "./calendar-directives.d.ts",
+                    },
+                    "ignoreMissingDependencies": true,
+                },
+            },
+        },
+        "nativescript-ui-chart": {
+            "entryPoints": {
+                ".": {
+                    "ignoreMissingDependencies": true,
+                },
+                "angular": {
+                    "override": {
+                        "main": "./chart-directives.js",
+                        "typings": "./chart-directives.d.ts",
+                    },
+                    "ignoreMissingDependencies": true,
+                },
+            },
+        },
+        "nativescript-ui-dataform": {
+            "entryPoints": {
+                ".": {
+                    "ignoreMissingDependencies": true,
+                },
+                "angular": {
+                    "override": {
+                        "main": "./dataform-directives.js",
+                        "typings": "./dataform-directives.d.ts",
+                    },
+                    "ignoreMissingDependencies": true,
+                },
+            },
+        },
+        "nativescript-ui-gauge": {
+            "entryPoints": {
+                ".": {
+                    "ignoreMissingDependencies": true,
+                },
+                "angular": {
+                    "override": {
+                        "main": "./gauges-directives.js",
+                        "typings": "./gauges-directives.d.ts",
+                    },
+                    "ignoreMissingDependencies": true,
+                },
+            },
+        },
+        "nativescript-ui-listview": {
+            "entryPoints": {
+                ".": {
+                    "ignoreMissingDependencies": true,
+                },
+                "angular": {
+                    "override": {
+                        "main": "./listview-directives.js",
+                        "typings": "./listview-directives.d.ts",
+                    },
+                    "ignoreMissingDependencies": true,
+                },
+            },
+        },
+        "nativescript-ui-sidedrawer": {
+            "entryPoints": {
+                ".": {
+                    "ignoreMissingDependencies": true,
+                },
+                "angular": {
+                    "override": {
+                        "main": "./side-drawer-directives.js",
+                        "typings": "./side-drawer-directives.d.ts",
+                    },
+                    "ignoreMissingDependencies": true,
+                },
+            },
+        },
+        "kinvey-nativescript-sdk": {
+            "entryPoints": {
+                ".": {
+                    "ignoreMissingDependencies": true,
+                },
+                "lib": {
+                    "angular": {
+                        "override": {
+                            "main": "./kinvey.module.js",
+                            "typings": "./kinvey.module.d.ts",
+                        },
+                        "ignoreMissingDependencies": true,
+                    },
+                    "ignoreMissingDependencies": true,
+                },
+            },
+        },
+        "nativescript-localize": {
+            "entryPoints": {
+                ".": {
+                    "override": {
+                        "main": "./localize.module.js",
+                        "typings": "./localize.module.d.ts",
+                    },
+                    "ignoreMissingDependencies": true,
+                },
+            }
+        },
+    }
+}
