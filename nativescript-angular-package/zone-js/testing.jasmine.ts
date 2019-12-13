6984c267760bd9ddff5389cd518e2d47e110311d
Stoyan Stratev <stoyan.stratev@progress.com>
2019-12-13 13:16:42 +0200

    fix: wrong import path in compat package
    https://github.com/NativeScript/nativescript-angular/issues/2059

--- nativescript-angular-package/zone-js/testing.jasmine.ts
+++ nativescript-angular-package/zone-js/testing.jasmine.ts
@@ -1,3 +1,3 @@
 // Bootstrap helper module for jasmine spec tests
 import "@nativescript/angular/platform";
-import "@nativescript/angular/dist/zone-nativescript.jasmine.js";
+import "@nativescript/angular/zone-js/dist/zone-nativescript.jasmine.js";
